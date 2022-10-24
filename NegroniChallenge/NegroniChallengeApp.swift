6e05bb4787bb9dd0636f05cffd82abb596772b18
Simon Bestler <68077359+bestler@users.noreply.github.com>
2022-10-24 12:42:44 +0200

    Initial Commit

---
+++ NegroniChallenge/NegroniChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegroniChallengeApp.swift
+//  NegroniChallenge
+//
+//  Created by Simon Bestler on 24.10.22.
+//
+
+import SwiftUI
+
+@main
+struct NegroniChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
