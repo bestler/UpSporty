f5a8321c2917d5ba1295cd4e5de2577be52c0da9
nrigoni22 <nrigoni22@fed.idserve.net>
2022-10-31 11:58:26 +0100

    Changes to today view logic and ui

--- NegroniChallenge/ViewModel/MainViewModel.swift
+++ NegroniChallenge/ViewModel/MainViewModel.swift
@@ -5,8 +5,9 @@
 //  Created by Nicola Rigoni on 25/10/22.
 //
 
-import Foundation
 import CoreData
+import Combine
+import SwiftUI
 
 enum TrainingType: String, CaseIterable {
     case exercise = "Exercise"
@@ -41,10 +42,38 @@ class MainViewModel: ObservableObject {
     @Published var todayTrainingSheet: [TrainingEntity] = []
     @Published var todayGoals: [GoalEntity] = []
     @Published var selectedToday: GoalEntity? = nil
+    @Published var goalToShow: GoalEntity? = nil
+    @Published var selectedResultsToday: [TrainingResultEntity] = []
+    @Published var todayResultFilter: PresentationDetent = .medium
+    @Published var selectedTraining: TrainingEntity? = nil
+    @Published var resultsToday: [TrainingResultEntity] = []
+    @Published var temporaryMillisecods: Int64 = 0
+    @Published var update: Bool = false
+    
+    
+    //MARK: Combine
+    private var cancellables = Set<AnyCancellable>()
     
     init() {
         getGoals()
-        getTodayGoals()
+        
+        $selectedToday
+            .combineLatest($todayGoals)
+            .map(filterToday)
+            .sink { [weak self] goal in
+                self?.goalToShow = goal
+                self?.getTodayTrainingFromTodayGoal(for: goal)
+            }
+            .store(in: &cancellables)
+        
+        $resultsToday
+            .combineLatest($todayResultFilter, $update)
+            .map(filterResultToday)
+            .sink { [weak self] results in
+                print("filter \(self?.todayResultFilter == .medium ? "medium" : "full")")
+                self?.selectedResultsToday = results
+            }
+            .store(in: &cancellables)
     }
     
     func checkSportSelection() -> Bool {
@@ -67,11 +96,11 @@ class MainViewModel: ObservableObject {
         newGoal.sportID = selectedSport
         guard let target = Double(target) else { return }
         newGoal.target = target
-        newGoal.targetTime = calculateMilliseconds()
+        newGoal.targetTime = calculateMilliseconds(hour: 0, minute: 1, second: 1, millisecond: 1)
     }
     //TODO: complete
-    private func calculateMilliseconds() -> Int64 {
-        return 100
+    func calculateMilliseconds(hour: Int, minute: Int, second: Int, millisecond: Int) -> Int64 {
+        return Int64((hour * 3600000) + (minute * 60000) + (second * 1000) + millisecond)
     }
     
     //TODO: complete
@@ -156,12 +185,30 @@ class MainViewModel: ObservableObject {
         let sort = NSSortDescriptor(keyPath: \TrainingResultEntity.number, ascending: true)
         request.sortDescriptors = [sort]
         
-        //print("get training sheet for \(goal.sportID)")
+        
         do {
             currentResultTraining = try manager.context.fetch(request)
         } catch let error {
             print("Error fetching coredata: \(error.localizedDescription)")
         }
+    }
+    
+    func updateResult(resultNumber: Int16, newResult: Int64, training: TrainingEntity, onSave: Bool) {
+        let resultNumber = onSave ? resultNumber : resultNumber - 1
+        if let index = selectedResultsToday.firstIndex(where: { $0.number == resultNumber }) {
+            selectedResultsToday[index].result = Int64(newResult)
+            training.repeatCountActual += 1
+            print("training result: \(selectedResultsToday[index].result)")
+            print("training array result: \(selectedResultsToday)")
+            update.toggle()
+        }
+    }
+    
+    
+    
+    func saveResults() {
+        saveResult()
+        getTodayGoals()
     }
     
     private func saveGoal() {
@@ -172,20 +219,11 @@ class MainViewModel: ObservableObject {
         self.manager.save()
         self.getGoals()
         self.getTodayGoals()
-        /*   businesses.removeAll()
-         deparments.removeAll()
-         employees.removeAll()
-         
-         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-         self.manager.save()
-         self.getBusinesses()
-         self.getDepartments()
-         self.getEmployees()
-         }
-         */
     }
     
-    private func getTodayGoals() {
+    func getTodayGoals() {
+        print("getTodayGoals")
+        selectedToday = nil
         let request = NSFetchRequest<GoalEntity>(entityName: "GoalEntity")
         
         let sort = NSSortDescriptor(keyPath: \GoalEntity.dueDate, ascending: true)
@@ -197,10 +235,9 @@ class MainViewModel: ObservableObject {
             let toFilter = NSPredicate(format: "ANY trainings.dueDate < %@", dateTo as CVarArg)
             let filter = NSCompoundPredicate(andPredicateWithSubpredicates: [fromFilter, toFilter])
             request.predicate = filter
-            
             do {
                 todayGoals = try manager.context.fetch(request)
-                getTodayTrainingFromTodayGoal(for: todayGoals.first)
+                selectedToday = todayGoals.first
             } catch let error {
                 print("Error fetching coredata: \(error.localizedDescription)")
             }
@@ -210,20 +247,71 @@ class MainViewModel: ObservableObject {
     private func getTodayTrainingFromTodayGoal(for todayGoal: GoalEntity?) {
         let request = NSFetchRequest<TrainingEntity>(entityName: "TrainingEntity")
         if let goal = todayGoal {
-            let filter = NSPredicate(format: "goal == %@", goal)
-            request.predicate = filter
-            do {
-                todayTrainingSheet = try manager.context.fetch(request)
-            } catch let error {
-                print("Error fetching coredata: \(error.localizedDescription)")
+            let dateFrom = Calendar.current.startOfDay(for: Date())
+            if let dateTo = Calendar.current.date(byAdding: .day, value: 1, to: dateFrom) {
+                let filterGoal = NSPredicate(format: "goal == %@", goal)
+                let fromFilter = NSPredicate(format: "ANY dueDate >= %@", dateFrom as CVarArg)
+                let toFilter = NSPredicate(format: "ANY dueDate < %@", dateTo as CVarArg)
+                let filter = NSCompoundPredicate(andPredicateWithSubpredicates: [filterGoal, fromFilter, toFilter])
+                request.predicate = filter
+                
+                let sort = NSSortDescriptor(keyPath: \TrainingEntity.dueDate, ascending: true)
+                request.sortDescriptors = [sort]
+                
+                do {
+                    todayTrainingSheet = try manager.context.fetch(request)
+                } catch let error {
+                    print("Error fetching coredata: \(error.localizedDescription)")
+                }
             }
         }
     }
+    
+    private func filterToday(selectedGoal: GoalEntity?, todayGoals: [GoalEntity]) -> GoalEntity? {
+        if let selectedGoal = selectedGoal {
+            return todayGoals.first(where: { $0.id == selectedGoal.id })
+        }
+        return todayGoals.first
+    }
+    
+    func getResultFromTodayTraining(for training: TrainingEntity) {
+        let request = NSFetchRequest<TrainingResultEntity>(entityName: "TrainingResultEntity")
+        let filter = NSPredicate(format: "training == %@", training)
+        request.predicate = filter
+        
+        let sort = NSSortDescriptor(keyPath: \TrainingResultEntity.number, ascending: true)
+        request.sortDescriptors = [sort]
+        
+        
+        do {
+            resultsToday = try manager.context.fetch(request)
+        } catch let error {
+            print("Error fetching coredata: \(error.localizedDescription)")
+        }
+    }
+    
+    private func filterResultToday(results: [TrainingResultEntity], filter: PresentationDetent, update: Bool) -> [TrainingResultEntity] {
+        print("inside filterResultToday")
+        if filter == .medium {
+            if let result = results.first(where: { $0.result == 0 }) {
+                print("filter today medium result: \(result)")
+                return [result]
+            }
+        }
+        print("filter today full")
+        return results.filter( { $0.result == 0 } )
+    }
+    
+    
  
     private func saveTraining(goal: GoalEntity) {
         currentTrainingSheet.removeAll()
         newTrainingStep.removeAll()
         self.manager.save()
         self.getTrainingSheet(for: goal)
+    }
+    
+    private func saveResult() {
+        self.manager.save()
     }
 }
